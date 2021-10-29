e6d4c99053e97fbf1450e1b0925c7ba8b22f2002
KenanMaslesa <kenoo.maslesa@gmail.com>
2021-10-29 15:36:34 +0200

    redirectTo subscription

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { SubscriptionComponent } from './subscription/subscription.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'subscription'},
   {
     path: 'subscription', component: SubscriptionComponent
   }
